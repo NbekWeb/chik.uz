7e921cb5e1481aa6473b42a9f4ad5a68b17aa73e
kudratov25 <qudratov25@mail.ru>
2024-04-07 16:41:57 +0500

    m

--- app/Http/Middleware/IsSuperUser.php
+++ app/Http/Middleware/IsSuperUser.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,7 +15,7 @@ class IsSuperUser
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-   public function handle($request, Closure $next)
+   public function handle(Request $request, Closure $next)
     {
         $user = Auth::user();
 
