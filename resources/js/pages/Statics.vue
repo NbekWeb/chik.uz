7e4133eb1902cf72e2e3e686ee6e31c64db443bf
kudratov25 <qudratov25@mail.ru>
2024-05-14 12:33:11 +0500

    pages updated

--- resources/js/pages/Statics.vue
+++ resources/js/pages/Statics.vue
@@ -1,47 +1,44 @@
 <template>
-
     <div class="container">
         <div>
-
-
-
             <div class="page__header">
                 <h2 class="diz">Статистика и аналитика</h2>
-
             </div>
+            <h2>{{ category }} </h2>
+            <section class="cards-blog latest-blog">
+                <div class="card" v-for="post in posts" :key="post.id">
+                    <img v-if="post.images.length > 0" :src="post.images[0]?.url" :alt="`First Image`" />
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="single-blog.html"></a>
+                            <router-link :to="{
+                                name: 'SingleBlog',
+                                params: { slug: post.slug },
+                            }">{{ post.title }}</router-link>
+                        </h5>
 
-
-            <div class="col-md-12">
-                <h2>{{ category }} </h2>
-                <div class="card_iten">
-                    <div class="card-blog-content" v-for="post in posts" :key="post.id">
-                        <img :src="post.imagePath" alt="" />
-                        <div class="item-content">
-
-                            <p class="item_title">
-                            <h3>{{ post.title }}</h3>
-                            </p>
-                            <div class="item-price">
-                                <h3>{{ post.price }}</h3> Uzs
-                            </div>
-
+                        <br>
+                        <h6 class="card-subtitle mb-2 text-end">{{ post.price }} Uzs</h6>
+                        <hr>
+                        <div class="card-user">
+                            <img :src="post.userImage ? post.userImage : '/assets/img/avatar.png'"
+                                style="width: 40px; height: 40px; border-radius: 50%;" />
+                            <h6>{{ post.user ? post.user : 'Unknown User' }}</h6>
                         </div>
-
-
                     </div>
                 </div>
 
-            </div>
-
-
-
+            </section>
+            <ul class="pagination">
+                <li v-for="(page, index) in pagination.links" :key="index" class="page-item"
+                    :class="{ 'active': page.active }">
+                    <a class="page-link" @click.prevent="fetchPage(page.url)" href="#">
+                        {{ page.label }}
+                    </a>
+                </li>
+            </ul>
         </div>
+
     </div>
-
-
-
-
-
 </template>
 
 <script>
@@ -51,14 +48,34 @@ export default {
     data() {
         return {
             posts: [],
+            pagination: {},
         };
     },
 
     mounted() {
-        this.filterByCategory('Статистика и аналитика'); // Initial load with category name 'Design'
+        axios
+            .get("/api/categories")
+            .then((response) => {
+                this.categories = response.data;
+                this.filterByCategory('Статистика и аналитика'); // Move this inside the categories fetch success block
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     },
-
     methods: {
+
+        fetchPage(url, category) {
+            axios.get(url, { params: { category } })
+                .then((response) => {
+                    this.posts = response.data.data; // Update posts data
+                    this.pagination = response.data.meta; // Update pagination data
+                    this.replaceLabels(); // Call replaceLabels here
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
         filterByCategory(name) {
             axios
                 .get("/api/posts", {
@@ -68,10 +85,21 @@ export default {
                 })
                 .then((response) => {
                     this.posts = response.data.data;
+                    this.pagination = response.data.meta;
+                    this.replaceLabels(); // Call replaceLabels here
                 })
                 .catch((error) => {
                     console.log(error);
                 });
+        },
+        replaceLabels() {
+            this.pagination.links.forEach(page => {
+                if (page.label === "&laquo; Previous") {
+                    page.label = "<<";
+                } else if (page.label === "Next &raquo;") {
+                    page.label = ">>";
+                }
+            });
         },
     },
 };
@@ -117,6 +145,35 @@ export default {
     color: #04b70a;
     font-size: 16px;
     font-weight: 700;
+}
+
+.card-subtitle {
+    color: #04b70a !important;
+}
+
+.card-user img {
+    /*flex-basis: 12%;*/
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+}
+
+.card-user h6 {
+    position: relative;
+    /*flex-basis: 80%;*/
+    left: 10px;
+}
+
+.card-user {
+    display: flex;
+
+}
+
+.card-title a {
+    font-weight: 400;
+    color: #111;
+    text-decoration: none;
+    list-style: none;
 }
 
 .diz {
--- resources/js/pages/Website Promotion.vue
+++ resources/js/pages/Website Promotion.vue
@@ -1,102 +1,134 @@
 <template>
-  
-  <div class="container">
-    <div >
-   
-   
-    
-   <div class="page__header">
-   <h2 class="diz">Продвижение сайта в топ</h2>
-   
- </div>
- 
- 
-   <div class="col-md-12">
- <h2>{{ category }} </h2>
- <div class="card_iten">
-  <div class="card-blog-content" v-for="post in posts" :key="post.id">
-   <img :src="post.imagePath" alt="" />
-   <div class="item-content">
-   
-      <p class="item_title">
-        <h3>{{ post.title }}</h3>
-      </p>
-      <div class="item-price">
-        <h3>{{ post.price }}</h3> Uzs
-      </div>
-   
-   </div>
-   
-   
- </div>
- </div>
- 
-</div>
- 
- 
+    <div class="container">
+        <div>
+            <div class="page__header">
+                <h2 class="diz">Продвижение сайта в топ</h2>
+            </div>
+            <h2>{{ category }} </h2>
+            <section class="cards-blog latest-blog">
+                <div class="card" v-for="post in posts" :key="post.id">
+                    <img v-if="post.images.length > 0" :src="post.images[0]?.url" :alt="`First Image`" />
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="single-blog.html"></a>
+                            <router-link :to="{
+                                name: 'SingleBlog',
+                                params: { slug: post.slug },
+                            }">{{ post.title }}</router-link>
+                        </h5>
 
-</div>
-  </div>
-    
-  
- 
+                        <br>
+                        <h6 class="card-subtitle mb-2 text-end">{{ post.price }} Uzs</h6>
+                        <hr>
+                        <div class="card-user">
+                            <img :src="post.userImage ? post.userImage : '/assets/img/avatar.png'"
+                                style="width: 40px; height: 40px; border-radius: 50%;" />
+                            <h6>{{ post.user ? post.user : 'Unknown User' }}</h6>
+                        </div>
+                    </div>
+                </div>
 
+            </section>
+            <ul class="pagination">
+                <li v-for="(page, index) in pagination.links" :key="index" class="page-item"
+                    :class="{ 'active': page.active }">
+                    <a class="page-link" @click.prevent="fetchPage(page.url)" href="#">
+                        {{ page.label }}
+                    </a>
+                </li>
+            </ul>
+        </div>
 
+    </div>
 </template>
 
 <script>
 export default {
-props: ['posts', 'category'],
+    props: ['posts', 'category'],
 
-data() {
-  return {
-    posts: [],
-  };
-},
+    data() {
+        return {
+            posts: [],
+            pagination: {},
+        };
+    },
 
-mounted() {
-  this.filterByCategory('Продвижение сайта в топ'); // Initial load with category name 'Design'
-},
+    mounted() {
+        axios
+            .get("/api/categories")
+            .then((response) => {
+                this.categories = response.data;
+                this.filterByCategory('Продвижение сайта в топ'); // Move this inside the categories fetch success block
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    methods: {
 
-methods: {
-  filterByCategory(name) {
-    axios
-      .get("/api/posts", {
-        params: {
-          category: name,
+        fetchPage(url, category) {
+            axios.get(url, { params: { category } })
+                .then((response) => {
+                    this.posts = response.data.data; // Update posts data
+                    this.pagination = response.data.meta; // Update pagination data
+                    this.replaceLabels(); // Call replaceLabels here
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         },
-      })
-      .then((response) => {
-        this.posts = response.data.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-},
+        filterByCategory(name) {
+            axios
+                .get("/api/posts", {
+                    params: {
+                        category: name,
+                    },
+                })
+                .then((response) => {
+                    this.posts = response.data.data;
+                    this.pagination = response.data.meta;
+                    this.replaceLabels(); // Call replaceLabels here
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        replaceLabels() {
+            this.pagination.links.forEach(page => {
+                if (page.label === "&laquo; Previous") {
+                    page.label = "<<";
+                } else if (page.label === "Next &raquo;") {
+                    page.label = ">>";
+                }
+            });
+        },
+    },
 };
 </script>
 
 <style>
-.right_side_menu{
-  color: rgb(0, 0, 0);
+.right_side_menu {
+    color: rgb(0, 0, 0);
 }
-.design_page{
-  display: flex;
-  margin-top: 40px;
- 
-}
-.col-md-8{
-  width: 60vw;
-}
-.page__header{
 
-margin: 40px 0 34px;
-  text-align: center;
+.design_page {
+    display: flex;
+    margin-top: 40px;
+
 }
-.item_title{
-  
-  color: #111;
+
+.col-md-8 {
+    width: 60vw;
+}
+
+.page__header {
+
+    margin: 40px 0 34px;
+    text-align: center;
+}
+
+.item_title {
+
+    color: #111;
     font-size: 14px;
     height: 36px;
     line-height: 18px;
@@ -105,30 +137,63 @@ margin: 40px 0 34px;
     min-height: auto;
     padding: 0;
 }
-.item-price{
-  
-  height: 50px;
+
+.item-price {
+
+    height: 50px;
     padding: 13px 0 0;
-  color: #04b70a;
+    color: #04b70a;
     font-size: 16px;
     font-weight: 700;
 }
-.diz{
-display: block;
-  font-size: 2em;
-  margin-block-start: 0.67em;
-  margin-block-end: 0.67em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  font-weight: bold;
+
+.card-subtitle {
+    color: #04b70a !important;
 }
-.card-blog-content{
-  box-sizing: border-box;
+
+.card-user img {
+    /*flex-basis: 12%;*/
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+}
+
+.card-user h6 {
+    position: relative;
+    /*flex-basis: 80%;*/
+    left: 10px;
+}
+
+.card-user {
+    display: flex;
+
+}
+
+.card-title a {
+    font-weight: 400;
+    color: #111;
+    text-decoration: none;
+    list-style: none;
+}
+
+.diz {
+    display: block;
+    font-size: 2em;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+}
+
+.card-blog-content {
+    box-sizing: border-box;
     padding: 6px;
     width: 25%;
 }
-.card_item{
-  -webkit-box-align: center;
+
+.card_item {
+    -webkit-box-align: center;
     -ms-flex-align: center;
     -webkit-box-pack: center;
     -ms-flex-pack: center;
@@ -142,14 +207,16 @@ display: block;
     margin: 13px -6px 0;
 
 }
-@media (min-width: 768px){
-  .rubrics_item{
-    width: 25%;
-  }
+
+@media (min-width: 768px) {
+    .rubrics_item {
+        width: 25%;
+    }
 }
-@media (max-width: 768px){
-  .rubrics_item{
-    width: 50%;
-  }
+
+@media (max-width: 768px) {
+    .rubrics_item {
+        width: 50%;
+    }
 }
 </style>
