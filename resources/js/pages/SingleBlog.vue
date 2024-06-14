a6bf546c220e22c05a548f03a9878dac29c61ecf
NbekWeb <nbekdev@gmail.com>
2024-06-14 17:24:27 +0500

    add spin to single blog

--- resources/js/pages/SingleBlog.vue
+++ resources/js/pages/SingleBlog.vue
@@ -1,112 +1,129 @@
 <template>
-    <div
-        class="container flex flex-wrap justify-between py-4 lg:flex-row max-lg:flex-col"
-    >
-        <div class="bg-white lg:w-[660px] max-lg:w-full">
-            <h3 class="px-4 py-3 text-2xl font-semibold text-black">
-                {{ post.title }}
-            </h3>
-            <Swiper
-                :slides-per-view="1"
-                spaceBetween="10"
-                :autoplay="{ delay: 3000 }"
-                :pagination="{ clickable: true }"
-                navigation
-                :modules="modul"
-                class="object-cover w-full sm:h-[440px] max-sm:h-[300px]"
-            >
-                <SwiperSlide v-for="(image, index) in post.images" :key="index">
-                    <img :src="image.url" class="object-cover w-full h-full" />
-                </SwiperSlide>
-            </Swiper>
-            <div v-html="post.body" class="px-4 py-2"></div>
-            <div class="flex justify-between px-4 pb-3">
-                <p class="text-xl font-bold text-black">
-                    Цена: {{ post.price }}
-                </p>
-                <a-button @click="buyPost(post.id)" type="primary">
-                    Связаться
-                </a-button>
-            </div>
-        </div>
-        <div>
-            <div
-                class="lg:w-[335px] max-h-[500px] max-lg:w-full lg:mt-0 max-lg:mt-4"
-            >
-                <div class="flex w-full gap-3 p-3 bg-white">
-                    <div class="items-center md:flex max-md:hidden">
-                        <img
-                            src="../images/garant.svg"
-                            class="w-[100px] h-[100px] "
-                        />
-                    </div>
-                    <div class="lg:w-[200px] max-lg:w-full">
-                        <h4 class="text-xl">Гарантия возврата</h4>
-                        <p class="text-xs text-text__grey">
-                            Средства моментально вернутся на счет, если что-то
-                            пойдет не так
-                        </p>
-                        <div
-                            class="flex items-center text-sm text-blue-600 cursor-pointer"
-                            @click="() => (desc = !desc)"
+    <div class="container min-h-screen">
+        <div
+            class="flex flex-wrap justify-between py-4 lg:flex-row max-lg:flex-col"
+        >
+            <div class="lg:w-[660px] max-lg:w-full">
+                <a-spin :spinning="loading" class="flex">
+                    <div class="bg-white">
+                        <h3 class="px-4 py-3 text-2xl font-semibold text-black">
+                            {{ post.title }}
+                        </h3>
+                        <Swiper
+                            :slides-per-view="1"
+                            spaceBetween="10"
+                            :autoplay="{ delay: 3000 }"
+                            :pagination="{ clickable: true }"
+                            navigation
+                            :modules="modul"
+                            class="object-cover w-full sm:h-[440px] max-sm:h-[300px]"
                         >
-                            Как это работает?
-                            <DownOutlined
-                                class="text-[10px] ml-1 ease-linear"
-                                :class="desc ? 'rotate-180' : 'rotate-0'"
-                            />
+                            <SwiperSlide
+                                v-for="(image, index) in post.images"
+                                :key="index"
+                            >
+                                <img
+                                    :src="image.url"
+                                    class="object-cover w-full h-full"
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+                        <div v-html="post.body" class="px-4 py-2"></div>
+                        <div class="flex justify-between px-4 pb-3">
+                            <p class="text-xl font-bold text-black">
+                                Цена: {{ post.price }}
+                            </p>
+                            <a-button @click="buyPost(post.id)" type="primary">
+                                Связаться
+                            </a-button>
                         </div>
                     </div>
-                </div>
-                <div class="p-3 bg-white" :class="desc ? 'block' : 'hidden'">
-                    <p class="text-xs">
-                        Kwork переводит деньги продавцу, только когда покупатель
-                        проверил и принял заказ.
-                    </p>
-                    <h4 class="text-xl font-bold">Деньги можно вернуть:</h4>
-                    <ul class="text-sm list-disc">
-                        <li>
-                            Моментально, если заказ отменяется покупателем в
-                            первые 20 мин. после старта
-                        </li>
-                        <li>
-                            Моментально, если продавец просрочил заказ, и
-                            покупатель решил отменить его
-                        </li>
-                        <li>
-                            Моментально, если продавец и покупатель согласовали
-                            отмену заказа
-                        </li>
-                        <li>
-                            В течение нескольких часов, если заказ выполнен
-                            некачественно или не полностью
-                        </li>
-                    </ul>
-                </div>
+                </a-spin>
             </div>
-            <div class="gap-2 p-3 mt-4 bg-white">
-                <div class="flex items-center">
-                    <img
-                        :src="
-                            post.userImage
-                                ? post.userImage
-                                : '/assets/img/avatar.png'
-                        "
-                        class="w-[60px] h-[60px] rounded-full"
-                    />
-
-                    <p class="text-xl font-semibold">{{ post.user }}</p>
-                </div>
-                <div class="flex items-center justify-between">
-                    <span class="">Репутация</span>
-                    <div class="flex items-center">
-                        <p class="pr-1 m-0 text-sm">5.0</p>
-                        <img
-                            src="../images/star.png"
-                            class="w-[15px] h-[15px] object-contain"
-                        />
+            <div>
+                <div
+                    class="lg:w-[335px] max-h-[500px] max-lg:w-full lg:mt-0 max-lg:mt-4"
+                >
+                    <div class="flex w-full gap-3 p-3 bg-white">
+                        <div class="items-center md:flex max-md:hidden">
+                            <img
+                                src="../images/garant.svg"
+                                class="w-[100px] h-[100px]"
+                            />
+                        </div>
+                        <div class="lg:w-[200px] max-lg:w-full">
+                            <h4 class="text-xl">Гарантия возврата</h4>
+                            <p class="text-xs text-text__grey">
+                                Средства моментально вернутся на счет, если
+                                что-то пойдет не так
+                            </p>
+                            <div
+                                class="flex items-center text-sm text-blue-600 cursor-pointer"
+                                @click="() => (desc = !desc)"
+                            >
+                                Как это работает?
+                                <DownOutlined
+                                    class="text-[10px] ml-1 ease-linear"
+                                    :class="desc ? 'rotate-180' : 'rotate-0'"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        class="p-3 bg-white"
+                        :class="desc ? 'block' : 'hidden'"
+                    >
+                        <p class="text-xs">
+                            Kwork переводит деньги продавцу, только когда
+                            покупатель проверил и принял заказ.
+                        </p>
+                        <h4 class="text-xl font-bold">Деньги можно вернуть:</h4>
+                        <ul class="text-sm list-disc">
+                            <li>
+                                Моментально, если заказ отменяется покупателем в
+                                первые 20 мин. после старта
+                            </li>
+                            <li>
+                                Моментально, если продавец просрочил заказ, и
+                                покупатель решил отменить его
+                            </li>
+                            <li>
+                                Моментально, если продавец и покупатель
+                                согласовали отмену заказа
+                            </li>
+                            <li>
+                                В течение нескольких часов, если заказ выполнен
+                                некачественно или не полностью
+                            </li>
+                        </ul>
                     </div>
                 </div>
+                <a-spin :spinning="loading">
+                    <div class="gap-2 p-3 mt-4 bg-white">
+                        <div class="flex items-center">
+                            <img
+                                :src="
+                                    post.userImage
+                                        ? post.userImage
+                                        : '/assets/img/avatar.png'
+                                "
+                                class="w-[60px] h-[60px] rounded-full"
+                            />
+
+                            <p class="text-xl font-semibold">{{ post.user }}</p>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <span class="">Репутация</span>
+                            <div class="flex items-center">
+                                <p class="pr-1 m-0 text-sm">5.0</p>
+                                <img
+                                    src="../images/star.png"
+                                    class="w-[15px] h-[15px] object-contain"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </a-spin>
             </div>
         </div>
     </div>
@@ -139,6 +156,7 @@ const error = ref(null);
 const currentUser = ref(null);
 const cash = ref("");
 const buying = ref(false);
+const loading = ref(true);
 const shouldHideAboutText = ref(false);
 
 // Router instance
@@ -167,10 +185,11 @@ const buyPost = async (postId) => {
         const response = await axios.post(`api/buy-order/${postId}`);
         const orderId = response.data.order_id;
         router.push(`/order/${orderId}`);
-
+        loading.value = true;
     } catch (err) {
         console.error("Purchase failed:", err);
     } finally {
+        loading.value = false;
         buying.value = false;
     }
 };
@@ -179,12 +198,14 @@ const fetchPostData = async () => {
     try {
         const response = await axios.get(`/api/posts/${props.slug}`);
         post.value = response.data.data;
-
         const isCurrentUserPostAuthor = post.value.user.id === Auth.id();
+        loading.value = true;
         shouldHideAboutText.value =
             Auth.isLoggedIn() && isCurrentUserPostAuthor;
     } catch (err) {
         console.log(err);
+    } finally {
+        loading.value = false;
     }
 };
 
