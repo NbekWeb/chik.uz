fae92cac8eb7507a03774efdde84de92c9ff17be
NbekWeb <nbekdev@gmail.com>
2024-07-08 22:18:23 +0500

    fix

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -1,61 +1,77 @@
 <template>
-    <section id="contact-us">
-        <h1>Будем на связи!</h1>
-
-        <!-- contact info -->
-        <div class="container">
-            <div class="contact-info">
-                <div class="specific-info">
-                    <i class="fas fa-home"></i>
-                    <div>
-                        <p class="title">Chik</p>
-                        <p class="subtitle">Tashkent</p>
-                    </div>
-                </div>
-                <div class="specific-info">
-                    <i class="fas fa-phone-alt"></i>
-                    <div>
-                        <a href="">+998 XXX XXX </a>
-                        <br />
-                        <a href="">+998 XXX XXX </a>
-
-                        <p class="subtitle"></p>
-                    </div>
-                </div>
-                <div class="specific-info">
-                    <i class="fas fa-envelope-open-text"></i>
-                    <div>
-                        <a href="mailto:info@alphayo.co.ke">
-                            <p class="title">info@chik.uz</p>
-                        </a>
-                        <p class="subtitle">Отправте нам</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Contact Form -->
-            <div class="contact-form">
-                <form action="" method="">
-                    <!-- Name -->
-                    <label for="name"><span>Имя</span></label>
-                    <input type="text" id="name" name="name" value="" />
-
-                    <!-- Email -->
-                    <label for="email"><span>Email</span></label>
-                    <input type="text" id="email" name="email" value="" />
-
-                    <!-- Subject -->
-                    <label for="subject"><span>Субъект</span></label>
-                    <input type="text" id="Subject" name="subject" value="" />
-
-                    <!-- Message -->
-                    <label for="message"><span>Текст</span></label>
-                    <textarea id="message" name="message"></textarea>
-
-                    <!-- Button -->
-                    <input type="submit" value="Отправить" />
-                </form>
+    <div class="container">
+        <p class="text-2xl text-black font-semibold pt-4 md:mb-16 max-md:mb-10">
+            Чтобы использовать банковскую карту Humo и Uzcard для оплаты, на
+            нужной странице следует нажать кнопку «Пополнить баланс». Это
+            позволит осуществить оплату через мобильный приложение ООО «Click» и
+            ИП OOO «INSPIRED» PAYME
+        </p>
+        <p class="text-2xl font-semibold mb-4">
+            Для оплаты нужно перейти: Логин > Админ > Платежи/транзакции >
+            выбрать: Click либо Payme > Оплатить
+        </p>
+        <div class="flex justify-center payment md:pb-10 max-md:pb-6">
+            <div class="">
+                <a-table
+                    :columns="columns"
+                    :data-source="data"
+                    bordered
+                    pagination="false"
+                >
+                </a-table>
             </div>
         </div>
-    </section>
+    </div>
 </template>
+
+<script setup>
+const columns = [
+    {
+        title: "Подпроцессор",
+        dataIndex: "name",
+        key: "name",
+    },
+    {
+        title: "Вид обработки",
+        dataIndex: "age",
+        key: "age",
+    },
+];
+
+const data = [
+    {
+        key: 1,
+        name: "Payme",
+        age: " Процессинг платежей",
+    },
+    {
+        key: 2,
+        name: "Click",
+        age: " Процессинг платежей",
+    },
+    {
+        key: 3,
+        name: "Octopay",
+        age: " Процессинг платежей",
+    },
+];
+</script>
+<style>
+.payment .ant-table-wrapper .ant-table-pagination.ant-pagination {
+    display: none !important;
+}
+
+.ant-table-wrapper .ant-table-thead > tr > th {
+    font-weight: 800 !important;
+}
+.ant-table-wrapper
+    .ant-table.ant-table-bordered
+    > .ant-table-container
+    > .ant-table-content
+    > table
+    > thead
+    > tr
+    > th {
+    padding: 16px 100px 16px 16px !important;
+}
+</style>
