aa5f70b699c4173c20baaef65b57658f16394c1b
NbekWeb <nbekdev@gmail.com>
2024-07-08 22:20:04 +0500

    fix

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -21,6 +21,7 @@
                 </a-table>
             </div>
         </div>
+        <p class="text-2xl font-medium mb-4">*Для выплаты платежей необходимо обратиться по эл. Почте info@chik.uz</p>
     </div>
 </template>
 
